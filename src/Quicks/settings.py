3a856c9cd7fe21c2c796cc5a4065e3a91445b678
kribst <ilassidjoba@gmail.com>
2024-09-29 15:17:26 +0100

    second

--- src/Quicks/settings.py
+++ src/Quicks/settings.py
@@ -11,19 +11,25 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 import os
 from pathlib import Path
+import environ
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = "django-insecure-rfp@=2m4!v9y9j(&8td&)07(t^+pp0femjsf_99ooy=khiv$u4"
+env = environ.Env()
+environ.Env.read_env(env_file =str(BASE_DIR / 'Quicks' / '.env'))  # reading.env file
+
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env.bool('DEBUG', False)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = env.list('ALLOWED_HOSTS')
 
-
+print(SECRET_KEY)
+print(ALLOWED_HOSTS)
+print(type(DEBUG))
 # Application definition
 
 INSTALLED_APPS = [
